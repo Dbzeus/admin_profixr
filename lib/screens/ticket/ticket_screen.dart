96ed59915e40fe3b7b8e16f9163caa46400073ad
Dinesh Raja <dineshraja1998k@mail.com>
2023-04-30 12:23:13 +0530

    ticket ui and ticket history partially finished

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:profixer_admin/routes/app_routes.dart';
 import 'package:profixer_admin/screens/ticket/ticket_controller.dart';
-import 'package:profixer_admin/widgets/custom_edittext.dart';
-import 'package:intl/intl.dart';
 
 import '../../helpers/custom_colors.dart';
-
 import '../../widgets/custom_appbar.dart';
-import '../../widgets/custom_button.dart';
 
 class TicketScreen extends GetView<TicketController> {
   final controller = Get.put(TicketController());
@@ -18,253 +16,264 @@ class TicketScreen extends GetView<TicketController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-        widget: const Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: EdgeInsets.only(bottom: 16.0),
-            child: Text(
-              "New Ticket",
-              style: TextStyle(
-                  color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold),
+        widget: Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: whiteColor,
+                    )),
+                const Text(
+                  "Tickets",
+                  style: TextStyle(
+                      color: whiteColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        padding:
-        const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 16),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 16.0,
+          right: 16.0,
+          left: 16.0,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-                hintText: "Customer Name",
-                controller: controller.customerNameController),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Customer Address",
-              controller: controller.customerAddressController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Customer Mobile Number",
-              controller: controller.customerMobileNoController,
-              prefixIcon: Obx(
-                    () =>
-                    DropdownButton(
-                        value: controller.mobileNoDropDownValue.value,
-                        style: const TextStyle(
-                            color: primaryColor, fontSize: 16),
-                        underline: const SizedBox(),
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: primaryColor,
-                          size: 16,
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(color: Colors.grey.shade100),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.shade100,
+                              spreadRadius: 1,
+                              blurRadius: 3,
+                              offset: const Offset(0, 2))
+                        ]),
+                    child: TextFormField(
+                      controller: controller.searchController,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: textColor,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: 'Search',
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: textColor,
                         ),
-                        items: controller.mobileItems.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                        onChanged: (val) {
-                          controller.mobileNoDropDownValue(val.toString());
-                        }),
-              ),
-            ),
-
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Service Category",
-              controller: controller.serviceCategoryController,
-              suffixIcon: Obx(
-                    () =>
-                    DropdownButton(
-                        value: controller.categoryDropDownValue.value,
-                        style: const TextStyle(color: blackColor, fontSize: 16),
-                        underline: const SizedBox(),
-                        isExpanded: true,
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: blackColor,
-                          size: 22,
-                        ),
-                        items: controller.categoryItems.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                        onChanged: (val) {
-                          controller.categoryDropDownValue(val.toString());
-                        }),
-              ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: primaryColor,
+                  ),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/icon/filter.svg',
+                      height: 12,
+                      width: 12,
+                    ),
+                  ),
+                )
+              ],
             ),
             const SizedBox(
-              height: 10,
+              height: 16,
             ),
-            CustomEditText(
-              hintText: "Service Name",
-              controller: controller.serviceNameController,
-              suffixIcon: Obx(
-                    () =>
-                    DropdownButton(
-                        value: controller.serviceNameDropDownValue.value,
-                        style: const TextStyle(color: blackColor, fontSize: 16),
-                        underline: const SizedBox(),
-                        isExpanded: true,
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: blackColor,
-                          size: 22,
-                        ),
-                        items: controller.serviceNameItems.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                        onChanged: (val) {
-                          controller.serviceNameDropDownValue(val.toString());
-                        }),
-              ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 6,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (_, index) {
+                    return _buildTickets();
+                  }),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Service Amount",
-              controller: controller.serviceAmountController,
-              prefixIcon: Obx(
-                    () =>
-                    DropdownButton(
-                        value: controller.amountDropDownValue.value,
-                        style: const TextStyle(
-                            color: primaryColor, fontSize: 16),
-                        underline: const SizedBox(),
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: primaryColor,
-                          size: 16,
-                        ),
-                        items: controller.amountItems.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                        onChanged: (val) {
-                          controller.amountDropDownValue(val.toString());
-                        }),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Service Type",
-              controller: controller.serviceTypeController,
-              suffixIcon: Obx(
-                    () =>
-                    DropdownButton(
-                        value: controller.serviceTypeDropDownValue.value,
-                        style: const TextStyle(color: blackColor, fontSize: 16),
-                        underline: const SizedBox(),
-                        isExpanded: true,
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: blackColor,
-                          size: 22,
-                        ),
-                        items: controller.serviceTypeItems.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                        onChanged: (val) {
-                          controller.serviceTypeDropDownValue(val.toString());
-                        }),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Service Date",
-              showCursor: false,
-              keyboardType: TextInputType.none,
-              controller: controller.serviceDateController,
-              suffixIcon: const Icon(
-                Icons.calendar_month_rounded,
-                color: blackColor,
-                size: 22,
-              ),
-              onTab: () async {
-                var date = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime(DateTime
-                        .now()
-                        .year + 1, 12, 31));
-                if (date != null) {
-                  debugPrint(date.toString());
-                   controller.serviceDateController.text =
-
-            DateFormat(controller.dateFormat)
-                .format(date);
-                }
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomEditText(
-              hintText: "Service Time Slot",
-              controller: controller.serviceTimeController,
-              showCursor: false,
-              keyboardType: TextInputType.none,
-              suffixIcon: const Icon(
-                Icons.keyboard_arrow_down,
-                color: blackColor,
-                size: 22,
-              ),
-              onTab: () async {
-                var time = await showTimePicker(
-                    context: context,
-                    initialTime: TimeOfDay.now());
-                if (time != null) {
-                  debugPrint(time.toString());
-                   controller.serviceTimeController.text =
-
-                      DateFormat(controller.timeFormat).format(
-                      DateTime(
-                          0, 1, 1, time.hour, time.minute));
-                }
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-
-            //const Spacer(),
-            CustomButton(
-                text: "Create",
-                onTap: () {
-                  //Get.toNamed(Routes.addCategory);
-                })
           ],
         ),
       ),
     );
+  }
+
+  _buildTickets() {
+    return GestureDetector(
+      onTap: () {
+        Get.toNamed(Routes.ticketHistory);
+      },
+      child: Container(
+          width: MediaQuery.of(Get.context!).size.width,
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(vertical: 6),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: Colors.grey.shade100),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade100,
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: const Offset(0, 2))
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Ticket Number, ",
+                        style: TextStyle(
+                            color: blackColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      const Text(
+                        "Place ",
+                        style: TextStyle(
+                            color: blackColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Technician Name",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: blackColor,
+                        ),
+                      ),
+                      const Text(
+                        "Technician Role",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: blackColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Customer Name",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: blackColor,
+                        ),
+                      ),
+                      const Text(
+                        "12345678901",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: blackColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    "Issue ",
+                    style: TextStyle(
+                        color: blackColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Date and Time",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: blackColor,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showDropDown();
+                        debugPrint("Icon");
+                      },
+                      icon: const Icon(
+                        Icons.more_vert,
+                        color: textColor,
+                      ))
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+
+  Widget showDropDown() {
+
+        return DropdownButton(
+        value: controller.dropDownValue.value,
+        style: const TextStyle(color: primaryColor, fontSize: 16),
+        underline: const SizedBox(),
+        icon: const Icon(
+          Icons.keyboard_arrow_down,
+          color: primaryColor,
+          size: 16,
+        ),
+        items: controller.items.map((String items) {
+          return DropdownMenuItem(
+            value: items,
+            child: Text(items),
+          );
+        }).toList(),
+        onChanged: (val) {
+          controller.dropDownValue(val.toString());
+        });
   }
 }
